58de4c2e352117a716f4a29f7428a9265f058ed7
CarlosCode368 <carlossarabia_r@hotmail.com>
2025-04-15 14:54:42 -0700

    Encapsulation

--- src/Store.java
+++ src/Store.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+class Store{
+    private double taxRate;
+    public Cart cart;
+    ArrayList<Customer> customers;
 
-public class Store {
+    Store(double taxRate) {
+        this.taxRate = taxRate;
+        this.cart = new Cart(this.taxRate);
+        this.customers = new ArrayList<Customer>();
+    }
+
+    public void add(Product p) {
+        this.cart.add(p);
+
+    }
+
+    public double getTotalWithTax() {
+        return (1 + taxRate) * this.cart.getTotal();
+    }
 }
